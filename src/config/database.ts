22f966faff7435cfec0d3b6d3f84008231e218c2
quanluong-manabie <lhongquan.1998@gmail.com>
2026-04-11 12:43:50 +0700

    Refactor connectToDatabase function to simplify connection logic and remove caching mechanism

--- src/config/database.ts
+++ src/config/database.ts
@@ -12,25 +12,54 @@ let connectionPromise: Promise<typeof mongoose> | null = null;
 mongoose.set("bufferCommands", false);
 mongoose.set("bufferTimeoutMS", 0);
 
+// export async function connectToDatabase() {
+//   if (
+//     cachedDb &&
+//     mongoose.connection.readyState === READY_STATE_CONNECTED
+//   ) {
+//     return Promise.resolve(cachedDb);
+//   }
+
+//   if (
+//     connectionPromise &&
+//     mongoose.connection.readyState === READY_STATE_CONNECTING
+//   ) {
+//     return connectionPromise;
+//   }
+
+//   cachedDb = null;
+//   connectionPromise = null;
+
+//   connectionPromise = mongoose
+//     .connect(EnvVariables.MONGODB_URI!, {
+//       dbName: EnvVariables.MONGODB_NAME!,
+//       serverSelectionTimeoutMS: 5000,
+//       socketTimeoutMS: 10000,
+//       maxPoolSize: 10,
+//     })
+//     .then((db) => {
+//       cachedDb = db;
+//       connectionPromise = null;
+//       // Only log connection success in development
+//       if (!isProduction) {
+//         logger.info(`Connected to MongoDB: ${EnvVariables.MONGODB_NAME}`);
+//       }
+//       return cachedDb;
+//     })
+//     .catch((err) => {
+//       connectionPromise = null;
+//       cachedDb = null;
+//       logger.error("MongoDB connection error:", err);
+//       throw err;
+//     });
+
+//   return connectionPromise;
+// }
+
+
+
 export async function connectToDatabase() {
-  if (
-    cachedDb &&
-    mongoose.connection.readyState === READY_STATE_CONNECTED
-  ) {
-    return Promise.resolve(cachedDb);
-  }
-
-  if (
-    connectionPromise &&
-    mongoose.connection.readyState === READY_STATE_CONNECTING
-  ) {
-    return connectionPromise;
-  }
-
-  cachedDb = null;
-  connectionPromise = null;
-
-  connectionPromise = mongoose
+  return mongoose
     .connect(EnvVariables.MONGODB_URI!, {
       dbName: EnvVariables.MONGODB_NAME!,
       serverSelectionTimeoutMS: 5000,
@@ -38,13 +67,11 @@ export async function connectToDatabase() {
       maxPoolSize: 10,
     })
     .then((db) => {
-      cachedDb = db;
-      connectionPromise = null;
       // Only log connection success in development
       if (!isProduction) {
         logger.info(`Connected to MongoDB: ${EnvVariables.MONGODB_NAME}`);
       }
-      return cachedDb;
+      return db;
     })
     .catch((err) => {
       connectionPromise = null;
@@ -53,7 +80,6 @@ export async function connectToDatabase() {
       throw err;
     });
 
-  return connectionPromise;
 }
 
 export default connectToDatabase;
